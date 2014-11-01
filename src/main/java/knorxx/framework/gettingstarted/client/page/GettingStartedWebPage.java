85633e4665833b7236cb095fe1aa8351d440e5f6
Jan Scheible <janScheible@users.noreply.github.com>
2014-11-01 22:22:42 +0100

    made the example page a bit more interesting ;-)

--- src/main/java/knorxx/framework/gettingstarted/client/page/GettingStartedWebPage.java
+++ src/main/java/knorxx/framework/gettingstarted/client/page/GettingStartedWebPage.java
@@ -3,6 +3,11 @@ package knorxx.framework.gettingstarted.client.page;
 import knorxx.framework.generator.web.client.WebPage;
 import org.springframework.stereotype.Component;
 import static org.stjs.javascript.Global.alert;
+import org.stjs.javascript.dom.Element;
+import org.stjs.javascript.jquery.Event;
+import static org.stjs.javascript.jquery.GlobalJQueryUI.$;
+import org.stjs.javascript.jquery.JQuery;
+import org.stjs.javascript.jquery.plugins.ButtonOptions;
 
 /**
  *
@@ -13,6 +18,14 @@ public class GettingStartedWebPage extends WebPage {
 
 	@Override
 	public void render() {
-		alert("I'm a Knorxx Framework application!");
-	}	
+		ButtonOptions buttonOptions = new ButtonOptions();
+		buttonOptions.label = "Click me";
+		
+		$(CONTAINER_ID)
+			.append((JQuery) $("<h1>I'm a Knorxx Framework application!</h1>"))
+			.append($("<div>").button(buttonOptions).click((Event event, Element elmnt) -> {
+				alert("Hello Knorxx!");
+				return true;
+			}));
+	}
 }
